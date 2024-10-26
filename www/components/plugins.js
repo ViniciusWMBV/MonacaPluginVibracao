e629f135710dc361564feca3649fe6a47f718360
ViniciusWMBV <viniciuswcontato@gmail.com>
2024-10-26 09:14:40 +0900

    Projetin do vini e leo

---
+++ www/components/plugins.js
@@ -0,0 +1,25 @@
+$(document).on("click","#alerta", function(){
+
+     function alertDismissed(){
+    }
+    navigator.notification.alert("aleatório", alertDismissed,"Aviso!","alerta");
+});
+
+$(document).on("click","#confirm", function(){
+    function confirma(buttonIndex){
+        if(buttonIndex == "1"){     
+        navigator.notification.alert("Escolheu a opção A");
+        }else{
+        navigator.notification.alert("Escolheu a opção B");
+        }
+    }
+    navigator.notification.confirm("Escolha A ou B", confirma,"Escolha",['A','B']);
+});
+
+$(document).on("click","#beep", function(){
+    navigator.notification.beep(4);
+});
+
+$(document).on("click","#vibra", function(){
+    navigator.vibrate([1000, 1000, 3000, 1000, 5000]);
+});
